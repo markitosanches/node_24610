80d8cb12030087e56a9436155c7ad0a11fe4f10b
Marcos <markitosanches@gmail.com>
2026-02-18 10:46:56 -0500

    Seance 12

---
+++ ecommerce/vue-app/src/components/MainFooter.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="w-full mx-auto  border-t border-gray-100 mt-1 max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">All Rights Reserved ©2026.</span>
+    </footer> 
+</template>
